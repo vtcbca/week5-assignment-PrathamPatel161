16113ab8953d5a156ec1d240643a1d3a1c0f1cbd
PrathamPatel161 <141404907+PrathamPatel161@users.noreply.github.com>
2023-08-17 14:46:51 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import csv
+
+
+# In[6]:
+
+
+with open('c:/sqlite3/csv/hello.txt',"r",errors="ignore")as f:
+    content=f.read()
+    print(content)
+
+
+# In[ ]:
+
+
+
+
